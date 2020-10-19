0aa957687dd2ca130255bf4c1f704e87f3d0b2b6
juyeon Lim <67691664+JaneJYLim@users.noreply.github.com>
2020-10-19 19:35:23 +0900

    change img + mainMenu와 subMenu 연동

--- asset/script/common.js
+++ asset/script/common.js
@@ -9,15 +9,31 @@ $("#main-menu>li, #sub-menu").mouseout(function () {
 //서브메뉴 구현 끝
 
 // gnb와 submenu hover 연동 시작
-const main_menu_a = document.querySelectorAll("#main-menu a");
-const sub_menu = document.querySelectorAll(".sub_menu");
-const sub_menu_a = document.querySelectorAll(".sub-menu a");
+let main_menu_a = document.querySelectorAll("#main-menu a");
+let sub_menu = document.querySelectorAll(".sub-menu");
+let sub_menu_a = document.querySelectorAll(".sub-menu a");
 
 //submenu를 mouseover했을 때 hover 적용
 for (let i = 0; i < sub_menu_a.length; i++) {
   sub_menu_a[i].addEventListener("mouseover", function () {
     this.classList.add("sub-menu-hover");
     this.style.color = "#009223";
+  });
+}
+
+// for(let i = 0; i < sub_menu.length; i++) {
+//   sub_menu[i].addEventListener("mouseover", function () {
+//     main_menu_a[i].style.color="#009223";
+//   })
+// }
+
+//submenu와  mainmenu연동
+for(let i = 0; i < sub_menu.length; i++) {
+  $(sub_menu).eq(i).mouseover(function() {
+    $(main_menu_a).eq(i).css({"color":"#009223"});
+  });
+  $(main_menu_a).eq(i).mouseover(function() {
+    $(main_menu_a).eq(i).css({"color":"#009223"});
   });
 }
 
@@ -33,4 +49,14 @@ for (let i = 0; i < sub_menu_a.length; i++) {
     }
   });
 }
+
+for(let i = 0; i < sub_menu.length; i++) {
+  $(sub_menu).eq(i).mouseout(function() {
+    $(main_menu_a).eq(i).css({"color":"#000"});
+  });
+  $(main_menu_a).eq(i).mouseout(function() {
+    $(main_menu_a).eq(i).css({"color":"#000"});
+  });
+}
+
 // gnb와 submenu hover 연동 끝
