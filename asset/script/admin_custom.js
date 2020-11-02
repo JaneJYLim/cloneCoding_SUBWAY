78e2412005bc1b753cbe7e6b9c5cb9ccc17c903d
juyeon Lim <67691664+JaneJYLim@users.noreply.github.com>
2020-11-02 12:42:30 +0900

    admin page 달력UI구현

--- asset/script/admin_custom.js
+++ asset/script/admin_custom.js
@@ -1,3 +1,4 @@
+//달력UI 구현
 
 $(".datepicker").datepicker({
   showOn: "both",
@@ -9,6 +10,20 @@ $(".datepicker").datepicker({
 
 $.datepicker.setDefaults({
   monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-  dayNames: ['일', '월', '화', '수', '목', '금', '토'],
   showMonthAfterYear: true
 });
+
+
+$(".datepicker2").datepicker({
+  showOn: "both",
+  buttonImage: "../images/search/calendar.png",
+  buttonImageOnly: true,
+  changeYear: true,
+  dateFormat: "mm-dd"
+});
+
+$.datepicker.setDefaults({
+  monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  showMonthAfterYear: true
+});
+
